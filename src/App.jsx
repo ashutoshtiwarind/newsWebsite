b61755effafb34fe47e60aa2c65daf863d4636a2
ashutosh tiwari <ashutosh.tiwari@freightdeck.com>
2022-08-30 01:09:17 +0530

    added app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Newspage from './components/Newspage';
+
+function App() {
+  return (
+      <>
+      <Newspage />
+      </>
+    );
+    
+    
+}
+
+export default App;
