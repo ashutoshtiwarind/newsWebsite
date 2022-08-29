ebf35b7db9a66e07dde0dfc8f06a2cf78bf88e7e
ashutosh tiwari <ashutosh.tiwari@freightdeck.com>
2022-08-30 01:07:23 +0530

    added Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import '../App.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <div>
+                hi
+            </div>
+            <div>
+                bye
+            </div>
+            <div>
+                why
+            </div>
+        </div>
+    );
+}
+export default Footer;
