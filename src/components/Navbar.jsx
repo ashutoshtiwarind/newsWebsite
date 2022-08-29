e3396d26903cba60a948cdcd0a1b56eae6a08e2e
ashutosh tiwari <ashutosh.tiwari@freightdeck.com>
2022-08-30 01:07:53 +0530

    added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import '../App.css'
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <div>
+                hi
+            </div>
+            <div>
+                bye
+            </div>
+            <div>
+                why
+            </div>
+        </div>
+    );
+}
+export default Navbar;
